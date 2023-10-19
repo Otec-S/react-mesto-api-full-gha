98536c2fb6bc95404c0176145cd3a56736e4074d
Sergey Grigorash <adviser@bk.ru>
2023-10-19 16:04:26 +0500

    fix: BASE_URL = "http://localhost:3000";

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.otec-s.students.nomoredomainsrocks.ru";
+export const BASE_URL = "http://localhost:3000";
 
 export function register(email, password) {
   return fetch(`${BASE_URL}/signup`, {
