9ea6039fdc81222853dc906d9e4f5554c88a99e5
Sergey Grigorash <adviser@bk.ru>
2023-10-19 23:29:11 +0500

    BASE_URL = "http://localhost:3000"

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://api.otec-s.students.nomoredomainsrocks.ru";
-// export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "https://api.otec-s.students.nomoredomainsrocks.ru";
+export const BASE_URL = "http://localhost:3000";
 
 export function register(email, password) {
   return fetch(`${BASE_URL}/signup`, {
